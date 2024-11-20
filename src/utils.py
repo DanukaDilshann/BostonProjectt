c01a896f6c0a0f83752691ce91e2cb87e562161b
Danuka Dilshan Rathnayaka <danukarathnayaka9@gmail.com>
2024-11-20 18:59:08 +0530

    'commit'

--- src/utils.py
+++ src/utils.py
@@ -8,19 +8,16 @@ import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-from src.exception import CustomException
 
 def save_object(file_path, obj):
-    try:
-        dir_path = os.path.dirname(file_path)
+    
+    dir_path = os.path.dirname(file_path)
 
-        os.makedirs(dir_path, exist_ok=True)
+    os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, "wb") as file_obj:
-            pickle.dump(obj, file_obj)
+    with open(file_path, "wb") as file_obj:
+        pickle.dump(obj, file_obj)
 
-    except Exception as e:
-        raise CustomException(e, sys)
     
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
@@ -51,7 +48,7 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         return report
 
     except Exception as e:
-        raise CustomException(e, sys)
+        pass
     
 def load_object(file_path):
     try:
@@ -59,4 +56,5 @@ def load_object(file_path):
             return pickle.load(file_obj)
 
     except Exception as e:
-        raise CustomException(e, sys)
+        pass
+    
